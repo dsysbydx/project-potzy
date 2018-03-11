479c5f4961f34444ca91a2bf6d0a9dc5011161ae
Saran Ahuja <Sunny@Sarans-MacBook-Pro.local>
2018-03-11 10:35:21 +0700

    add load price from yahoo, fix metadata loader from set

--- potzy/setdatareader.py
+++ potzy/setdatareader.py
@@ -1,8 +1,12 @@
 
 import pandas as pd
+import requests
+import string
 from urllib.parse import urlencode
 
 SET_PRICE_URL='https://www.set.or.th/set/historicaltrading.do?'
+SET_META_URL_MAIN = 'https://www.set.or.th/dat/eod/listedcompany/static/listedCompanies_en_US.xls'
+OUTPUT_META_FILE = '../data/meta/thai_stocks.csv'
 
 def load_price_data_from_set(ticker):
     df=pd.DataFrame()
@@ -19,4 +23,23 @@ def load_price_data_from_set(ticker):
     df.columns=[x.lower() for x in df.columns]
     
     return df
+
+
+def load_all_tickers(save_to_csv=False):
+
+    stock_table = pd.read_html(SET_META_URL_MAIN)[0]
+
+    #first row is empthy, second row is the column
+    columns = stock_table.iloc[1]
+    stock_table = stock_table.iloc[2:]
+
+    #set column, index
+    stock_table.columns = [x.lower() for x in columns]
+    stock_table.set_index('symbol',inplace=True)
+    stock_table.index.name='ticker'
+
+    if save_to_csv:
+        stock_table.to_csv(OUTPUT_META_FILE)
+
+    return stock_table
     
